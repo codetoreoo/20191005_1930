0c89ac45991077522107381ee53cc27b740196bd
Judy <codetoreoo@gmail.com>
2019-09-26 14:07:38 +0800

    NLog Sample OK Version ( Base )

---
+++ Source/Repos/WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.LayoutRenderers;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+ 
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //寫法一
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+
+            //寫法二 :  呼叫 Log() method            
+            logger.Log(LogLevel.Info, "Sample informational message");
+            
+
+        }
+    }
+
+}
